21aee80b2dd450e88327c1e9e7cf4bf1f9da6199
octitesta <octis.testa@gmail.com>
2020-05-14 16:02:39 -0300

    Clases Autor y Libro creadas

--- config/biblioteca/models.py
+++ config/biblioteca/models.py
@@ -3,8 +3,16 @@ from django.db import models
 # Create your models here.
 class Autor(models.Model):
     nombre = models.CharField(max_length=20)
-    codigo = models.IntegerField
-
+    codigo = models.AutoField(primary_key=True)
     def __str__(self):
-        return '{}'.format(self.nombre,self.codigo)
-        
+        return '{} {}'.format(self.nombre,self.codigo)
+
+
+class Libro(models.Model):
+    codigo = models.AutoField(primary_key=True)
+    codigo_autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=30)
+    editorial = models.CharField(max_length=30)
+    paginas = models.IntegerField
+    def __str__(self):
+        return '{} {}'.format(self.codigo,self.titulo)
