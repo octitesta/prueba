ffb80f12b705920ac487cbb4e3a7077776496376
octitesta <octis.testa@gmail.com>
2020-06-24 15:44:06 -0300

    Filtros hechos

--- config/biblioteca/admin.py
+++ config/biblioteca/admin.py
@@ -3,7 +3,39 @@ from django.contrib import admin
 # Register your models here.
 from. models import *
 
-admin.site.register(Autor)
-admin.site.register(Libro)
-admin.site.register(Ejemplar)
-admin.site.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    #exclude = ['codigo', 'telefono']
+    fieldsets = (
+        ('Datos', {
+            'fields': ('nombre',)
+        }),
+        ('Contacto', {
+            'fields': ('telefono','direccion')
+        }),
+    )
+    list_display = ['nombre', 'telefono', 'direccion']
+    
+
+class EjemplarAdmin(admin.ModelAdmin):
+    list_filter = ['libro',]
+    
+
+class LibroAdmin(admin.ModelAdmin):
+    #filter_vertical = ('titulo',)
+    list_display = ['titulo', 'editorial',]
+    
+
+class LibroInline(admin.TabularInline):
+    model = Libro
+
+class AutorAdmin(admin.ModelAdmin):
+    #list_display = []
+    inlines = [LibroInline,]
+    
+    search_fields = ['nombre','libro__titulo','libro__editorial']
+
+
+admin.site.register(Autor,AutorAdmin)
+admin.site.register(Libro,LibroAdmin)
+admin.site.register(Ejemplar,EjemplarAdmin)
+admin.site.register(Usuario, UsuarioAdmin)
